4124a24d43c798f53e387cf335144bdd21de2913
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-11-15 22:12:02 +0100

    Adding first version of CBWindow

---
+++ CB-CCL/CBWindow.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CB_CCL
+{
+    public class CBWindow : Window
+    {
+        #region Click events
+        protected void MinimizeClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        protected void RestoreClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = (WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        protected void CloseClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        #endregion
+
+        static CBWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CBWindow),
+                new FrameworkPropertyMetadata(typeof(CBWindow)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            Button minimizeButton = GetTemplateChild("minimizeButton") as Button;
+            if (minimizeButton != null)
+                minimizeButton.Click += MinimizeClick;
+
+            Button restoreButton = GetTemplateChild("restoreButton") as Button;
+            if (restoreButton != null)
+                restoreButton.Click += RestoreClick;
+
+            Button closeButton = GetTemplateChild("closeButton") as Button;
+            if (closeButton != null)
+                closeButton.Click += CloseClick;
+
+            base.OnApplyTemplate();
+        }
+
+        private void moveRectangle_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
